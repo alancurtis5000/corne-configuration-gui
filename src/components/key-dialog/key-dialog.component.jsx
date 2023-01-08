7d22412c26b9c1e1fe3f11c162230f1baa3de8bd
Alan <alancurtis5000+accounts@gmail.com>
2023-01-08 10:20:21 -0700

    adding more to dialog

--- src/components/key-dialog/key-dialog.component.jsx
+++ src/components/key-dialog/key-dialog.component.jsx
@@ -1,11 +1,15 @@
-import * as React from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import Button from "@mui/material/Button";
 
 export const KeyDialog = (props) => {
   const { onClose, selectedValue, isOpen } = props;
+  const { index, label, tapped, held } = selectedValue;
 
   const handleClose = () => {
     onClose(selectedValue);
@@ -13,7 +17,33 @@ export const KeyDialog = (props) => {
 
   return (
     <Dialog onClose={handleClose} open={isOpen}>
-      <DialogTitle>Set backup account</DialogTitle>
+      <DialogTitle>
+        Key {index + 1} Label {label}
+      </DialogTitle>
+      <DialogContent>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <DialogContentText>When</DialogContentText>{" "}
+          <Button onClick={handleClose}>Tapped</Button>
+          <DialogContentText>:</DialogContentText> {tapped}
+          <Button onClick={handleClose}>Clear</Button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <DialogContentText>When</DialogContentText>{" "}
+          <Button onClick={handleClose}>Held</Button>
+          <DialogContentText>:</DialogContentText> {held}
+          <Button onClick={handleClose}>Clear</Button>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
