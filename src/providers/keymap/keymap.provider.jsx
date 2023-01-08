8d1282f09201b0d7e906fbba9cb0494a68aca2a9
Alan <alancurtis5000+accounts@gmail.com>
2023-01-07 21:46:11 -0700

    updated provider with keymap

--- src/providers/keymap/keymap.provider.jsx
+++ src/providers/keymap/keymap.provider.jsx
@@ -1,33 +1,42 @@
 import React, { createContext, useState } from "react";
 import propTypes from "prop-types";
 
-import { incrementCount, decrementCount } from "./keymap.utils";
+// import { incrementCount, decrementCount } from "./keymap.utils";
 
 // can I and should I right test for this?
 /* istanbul ignore next */
 export const KeymapContext = createContext({
-  count: 0,
-  margin: 1,
+  keymap: [
+    {
+      label: "DEF",
+      bindings: `
+   &none   &kp Q &kp W &kp E &kp R &kp T          &kp Y &kp U  &kp I    &kp O   &kp P    &none
+   &none   &kp A &kp S &kp D &hm LCMD F &kp G     &kp H &hm LCMD J  &kp K     &kp L   &kp SEMI     &none
+   &none   &kp Z &kp X &kp C &kp V &kp B          &kp N &kp M  &kp COMMA &kp DOT   &kp FSLH    &none 
+                &kp LSHIFT  &kp SPACE &kp N3      &to ARROWS_L  &kp ENTER  &kp BACKSPACE
+                `,
+    },
+  ],
   // actions
-  increment: () => {},
-  decrement: () => {},
-  updateMargin: () => {},
-  reset: () => {},
+  // increment: () => {},
 });
 
 // can I and should I right test for this?
 /* istanbul ignore next */
 export const KeymapProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
-  const [margin, setMargin] = useState(1);
+  const [keymap, setKeymap] = useState([
+    {
+      label: "DEF",
+      bindings: `
+   &none   &kp Q &kp W &kp E &kp R &kp T          &kp Y &kp U  &kp I    &kp O   &kp P    &none
+   &none   &kp A &kp S &kp D &hm LCMD F &kp G     &kp H &hm LCMD J  &kp K     &kp L   &kp SEMI     &none
+   &none   &kp Z &kp X &kp C &kp V &kp B          &kp N &kp M  &kp COMMA &kp DOT   &kp FSLH    &none 
+                &kp LSHIFT  &kp SPACE &kp N3      &to ARROWS_L  &kp ENTER  &kp BACKSPACE
+                `,
+    },
+  ]);
   // actions
-  const increment = () => setCount(incrementCount(count, margin));
-  const decrement = () => setCount(decrementCount(count, margin));
-  const updateMargin = (updateMargin) => setMargin(updateMargin);
-  const reset = () => {
-    setCount(0);
-    setMargin(1);
-  };
+  // const increment = () => setCount(incrementCount(count, margin));
 
   // if count changes do something
   // useEffect(() => {
@@ -37,12 +46,8 @@ export const KeymapProvider = ({ children }) => {
   return (
     <KeymapContext.Provider
       value={{
-        count,
-        margin,
-        increment,
-        decrement,
-        updateMargin,
-        reset,
+        //increment
+        keymap,
       }}
     >
       {children}
