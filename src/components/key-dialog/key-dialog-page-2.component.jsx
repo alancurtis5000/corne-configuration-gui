084491ac726821b724b030f05ef1a81684c45dbe
Alan <alancurtis5000+accounts@gmail.com>
2023-01-08 12:53:34 -0700

    moves styles to styles file

--- src/components/key-dialog/key-dialog-page-2.component.jsx
+++ src/components/key-dialog/key-dialog-page-2.component.jsx
@@ -6,43 +6,37 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import Button from "@mui/material/Button";
 import { ModifierList } from "../modifier-list/modifier-list.component";
+import "./key-dialog-page-2.styles.scss";
 
 export const KeyDialogPage2 = (props) => {
   const { selectedValue, setPage } = props;
-  const { index } = selectedValue;
+  const { index, label } = selectedValue;
 
   const backToHome = () => {
     setPage(1);
   };
 
-  const currentValue = {
-    backgroundColor: "pink",
-    height: "100px",
-    width: "100px",
-  };
+  const currentValue = {};
+
   return (
-    <>
+    <div className="key-dialog-page-2">
       <DialogTitle>Key: {index + 1} (Edit Tapped) </DialogTitle>
       <DialogContent dividers>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
+        <div className="content">
           <div>
             <Button onClick={backToHome}> Change</Button>
             <Button onClick={backToHome}> Back</Button>
           </div>
-          <div style={currentValue}></div>
+          <div className="current-value" style={currentValue}>
+            {label}
+          </div>
 
           <DialogContentText>Add Modifiers</DialogContentText>
           <ModifierList />
           <DialogContentText>Description</DialogContentText>
         </div>
       </DialogContent>
-    </>
+    </div>
   );
 };
 
