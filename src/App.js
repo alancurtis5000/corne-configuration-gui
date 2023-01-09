306669c959fdc97465ab7909b50ef35ccde58ddc
Alan <alancurtis5000+accounts@gmail.com>
2023-01-08 17:39:17 -0700

    json db working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import './App.scss';
 import { MainPage } from './components/main-page/main-page.component';
 import { LayerTabs } from './components/layer-tabs/layer-tabs.component';
 import { KeymapProvider } from './providers/keymap/keymap.provider';
-
+import axios from 'axios'
 
 
 function App() {
+
+  const apiCall = () => {
+    axios
+      .get("/collections")
+      .then((res) => {
+        console.log(res.data)
+
+      }).catch(err => { console.log({ err }) })
+  }
+
+  apiCall()
+
   return (
     <div className="App">
       <KeymapProvider>
