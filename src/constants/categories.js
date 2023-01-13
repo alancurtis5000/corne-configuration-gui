5b8b23a2be0fb6672a9fdafc320e4ec578aa927e
Alan <alancurtis5000+accounts@gmail.com>
2023-01-12 23:26:51 -0700

    added spacing to keys and categories

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,25 +1,35 @@
 export const categories = [
-    {
-        "id": 61,
-        "code": "letters",
-        "name": "English",
-        "created_at": "2021-10-26T01:56:10.566Z",
-        "updated_at": "2021-10-26T01:56:44.063Z",
-        "url": null,
-        "description": null,
-        "priority": 0,
-        "deleted": false
-    },
-    {
-        "id": 63,
-        "code": "modifier",
-        "name": "Modifiers",
-        "created_at": "2021-10-26T01:56:10.584Z",
-        "updated_at": "2021-12-27T00:35:45.822Z",
-        "url": null,
-        "description": null,
-        "priority": 40,
-        "deleted": false
-    },
-
-]
+  {
+    id: 61,
+    code: "letters",
+    name: "English",
+    created_at: "2021-10-26T01:56:10.566Z",
+    updated_at: "2021-10-26T01:56:44.063Z",
+    url: null,
+    description: null,
+    priority: 0,
+    deleted: false,
+  },
+  {
+    id: 63,
+    code: "modifier",
+    name: "Modifiers",
+    created_at: "2021-10-26T01:56:10.584Z",
+    updated_at: "2021-12-27T00:35:45.822Z",
+    url: null,
+    description: null,
+    priority: 40,
+    deleted: false,
+  },
+  {
+    id: 81,
+    code: "spacing",
+    name: "Spacing",
+    created_at: "2021-10-26T01:56:10.711Z",
+    updated_at: "2021-12-27T00:35:45.817Z",
+    url: null,
+    description: null,
+    priority: 39,
+    deleted: false,
+  },
+];
