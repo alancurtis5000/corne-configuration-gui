c46ac8fa3ee47be90053d15fba22969afa65c192
Alan <alancurtis5000+accounts@gmail.com>
2023-01-19 22:41:47 -0700

    roughed in header with placeholder select

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { MainPage } from "./components/main-page/main-page.component";
 import { LayerTabs } from "./components/layer-tabs/layer-tabs.component";
 import { KeymapProvider } from "./providers/keymap/keymap.provider";
 import { BottomActions } from "./components/bottom-actions/bottom-actions.componet";
+import { Header } from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
       <KeymapProvider>
+        <Header />
         <MainPage>
           <LayerTabs />
-          <BottomActions />
         </MainPage>
+        <BottomActions />
       </KeymapProvider>
     </div>
   );
