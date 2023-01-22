74e58264d2d1ee6beb0b1c16e10de3e107b29930
Alan <alancurtis5000+accounts@gmail.com>
2023-01-21 23:21:35 -0700

    adding layout page

---
+++ src/pages/layout/layout.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+export const Layout = () => {
+  useEffect(() => {
+    // get layout
+  }, []);
+
+  return (
+    <div className="page">
+      <div>Layout page</div>
+    </div>
+  );
+};
