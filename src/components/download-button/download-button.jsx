97f9c374052d50fbdb73846e22ec709f668155f5
Alan <alancurtis5000+accounts@gmail.com>
2023-01-26 22:44:50 -0700

    extracted blob content into file

--- src/components/download-button/download-button.jsx
+++ src/components/download-button/download-button.jsx
@@ -3,95 +3,17 @@ import { IconButton } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 import { LayoutContext } from "../../providers/layout/layout.provider";
 import { saveAs } from "file-saver";
-import { generateBindingCode } from "../../utilities/generate-binding-code";
-import { addSpacingToKeyCode } from "../../utilities/data-parsing";
+import { generateBlob } from "./blob-content";
 
 export const DownloadButton = () => {
   const {
     layout: { layers },
   } = useContext(LayoutContext);
 
-  const generateConfigFile = () => {
-    const defineLayers = [];
-    const layerConfigs = [];
-
-    layers.forEach((layer) => {
-      const bindingsConfig = [];
-      defineLayers.push(`#define ${layer.label} ${layer.index} `);
-      layer.bindings.forEach((binding) => {
-        let tapBindingCode = undefined;
-
-        tapBindingCode = generateBindingCode(binding);
-        tapBindingCode = addSpacingToKeyCode(tapBindingCode);
-
-        if (binding.index === 0) {
-          bindingsConfig.push(`     &none            ${tapBindingCode}`);
-        } else if (binding.index === 9 || binding.index === 19) {
-          bindingsConfig.push(
-            ` ${tapBindingCode} &none \n          &none           `
-          );
-        } else if (binding.index === 29) {
-          bindingsConfig.push(
-            ` ${tapBindingCode} &none \n                                           `
-          );
-        } else {
-          bindingsConfig.push(` ${tapBindingCode}`);
-        }
-      });
-      layerConfigs.push(`${layer.label}_layer {
-        label = "${layer.label}";
-        bindings = <
-     ${bindingsConfig.join("")}
-        >;
-      };
-        `);
-    });
-
-    const layerConfigsToString = layerConfigs.join("\n");
-    const defineLayersToString = defineLayers.join("\n");
-
-    return { defineLayersToString, layerConfigsToString };
-  };
-
-  const defaultConfigTop = `
-  /*
-  * Copyright (c) 2020 The ZMK Contributors 
-  * SPDX-License-Identifier: MIT 
-  */ 
-  
-  #include <behaviors.dtsi> 
-  #include <dt-bindings/zmk/keys.h> 
-  #include <dt-bindings/zmk/bt.h> \n`;
   const handleDownload = () => {
-    const { defineLayersToString, layerConfigsToString } = generateConfigFile();
-    var blob = new Blob(
-      [
-        `${defaultConfigTop}
-${defineLayersToString}
-
-/ {
-  behaviors {
-    hm: homerow_mods {
-        compatible = "zmk,behavior-hold-tap";
-        label = "HOMEROW_MODS";
-        #binding-cells = <2>;
-        tapping-term-ms = <300>;
-        quick-tap-ms = <200>;
-        flavor = "tap-preferred";
-        bindings = <&kp>, <&kp>;
-    };
-};
-  keymap {
-compatible = "zmk,keymap";\n
-${layerConfigsToString}
-  };
-};
-    `,
-      ],
-      {
-        type: "text/plain;charset=utf-8",
-      }
-    );
+    var blob = new Blob(generateBlob(layers), {
+      type: "text/plain;charset=utf-8",
+    });
     saveAs(blob, "corne.keymap");
   };
 
