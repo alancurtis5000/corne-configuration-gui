65a6469d4d41115c46a6735069bf9374821712a1
Alan <alancurtis5000+accounts@gmail.com>
2023-01-31 23:16:59 -0700

    hide save buttons based on location

--- src/components/bottom-actions/bottom-actions.componet.jsx
+++ src/components/bottom-actions/bottom-actions.componet.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import SaveIcon from "@mui/icons-material/Save";
 import Button from "@mui/material/Button";
 import "./bottom-actions.styles.scss";
 import { DownloadButton } from "../download-button/download-button";
 import { LayoutContext } from "../../providers/layout/layout.provider";
+import { useLocation } from "react-router-dom";
 
 export const BottomActions = () => {
   const { saveLayout } = useContext(LayoutContext);
+  const location = useLocation();
 
   const handleSave = () => {
     saveLayout();
@@ -14,10 +16,14 @@ export const BottomActions = () => {
 
   return (
     <div className="bottom-actions">
-      <Button onClick={handleSave} startIcon={<ArrowBackIcon />}>
-        Save
-      </Button>
-      <DownloadButton />
+      {location.pathname !== "/" && (
+        <>
+          <Button onClick={handleSave} startIcon={<SaveIcon />}>
+            Save
+          </Button>
+          <DownloadButton />
+        </>
+      )}
     </div>
   );
 };
